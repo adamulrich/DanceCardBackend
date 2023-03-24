c35ef345d22c8654f4f2ba66c1749e3d41dc7831
meagan-roberts <meaganr@wc-3.com>
2023-03-23 22:09:53 -0600

    region jest test

--- models/region.js
+++ models/region.js
@@ -22,7 +22,21 @@ const regionExample = {
     regionId: 1,
     signingPassword: "DanceMachine"
 }
+async function doesRegionIdExist(regionId) {
+    //console.log(regionId);
+    try {
+        const id = await regionModel.findOne({"regionId": regionId});
+        console.log("ID: " + JSON.stringify(id));
+        if (id == null) {
+            return false;
+        } else {
+            return true;
+        }
+    } catch {
+        return false;    
+    }
+}
 
 const regionModel = mongoose.model("region", regionSchema, 'region');
 
-module.exports = {regionModel, regionExample};
+module.exports = {regionModel, regionExample, doesRegionIdExist};
